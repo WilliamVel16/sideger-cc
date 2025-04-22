e4bc7e91046b6882c4c9877a37e2538b1cf573ef
William Velasco <william.velasco@correounivalle.edu.co>
2025-04-22 10:37:21 -0500

    script run containers - and fix ssh script

---
+++ src-tauri/src/scripts/run_containers.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+source ../../../.env
+docker container run -d --rm -it --name ${CM_C} --net ${NET_NAME} --hostname ${CENTRAL_MANAGER_NAME} ${CM_I}
+docker container run -d --rm -it --name ${EXE_C} --net ${NET_NAME} --hostname ${EXECUTE_HOSTNAME} ${EXE_I}
+docker container run -d --rm -it --name ${EXE_C2} --net ${NET_NAME} --hostname ${EXECUTE_HOSTNAME_TWO} ${EXE_I}
+docker container run -d --rm -it --name ${EXE_C3} --net ${NET_NAME} --hostname ${EXECUTE_HOSTNAME_THREE} ${EXE_I}
+# cp htcondor examples
+docker container run -d --rm -it -v /home/wvel/Documentos/GW/htcondor-clus/htcondor_examples:/htcondor_examples --name ${SUB_C} --net ${NET_NAME} --hostname ${SUBMIT_HOSTNAME} ${SUB_I}
--- src-tauri/src/scripts/ssh_connection.sh
+++ src-tauri/src/scripts/ssh_connection.sh
@@ -4,6 +4,6 @@ for i in ${CM_C} ${SUB_C} ${EXE_C} ${EXE_C2} ${EXE_C3}; do
   echo -n "Preparando conexión SSH con ${i}... " 
   docker container exec ${i} sh -c "echo ${USER_PASSWORD} | sudo -S service ssh start"
   IP=$(docker inspect -f '{{.NetworkSettings.Networks.sidegernet.IPAddress}}' ${i})
-  copy-ssh-id user@$IP
+  sshpass -p ${USER_PASSWORD} ssh-copy-id -o StrictHostKeyChecking=no user@$IP
   echo Ok!
 done
