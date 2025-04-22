e2096e12a87361b086673dc56915e7a040fa58a5
William Velasco <william.velasco@correounivalle.edu.co>
2025-04-22 09:47:39 -0500

    script ssh connection

---
+++ src-tauri/src/scripts/ssh_connection.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+source ../../../.env
+for i in ${CM_C} ${SUB_C} ${EXE_C} ${EXE_C2} ${EXE_C3}; do
+  echo -n "Preparando conexión SSH con ${i}... " 
+  docker container exec ${i} sh -c "echo ${USER_PASSWORD} | sudo -S service ssh start"
+  IP=$(docker inspect -f '{{.NetworkSettings.Networks.sidegernet.IPAddress}}' ${i})
+  copy-ssh-id user@$IP
+  echo Ok!
+done
