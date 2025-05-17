2d6a8c9d63fc908a88115f8a1248b63bd275f487
William Velasco <william.velasco@correounivalle.edu.co>
2025-05-17 18:27:54 -0500

    -b refactoring to do a test migration

--- src-tauri/src/scripts/utils/get_resources_info.sh
+++ src-tauri/src/scripts/utils/get_resources_info.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-source ../.env
+source ../../.env
 echo "["
 FIRST=true
 for i in ${CM_C} ${SUB_C} ${EXE_C} ${EXE_C2} ${EXE_C3}; do
   IP=$(docker inspect -f '{{.NetworkSettings.Networks.sidegernet.IPAddress}}' ${i})
-  INFO_NODE=$(ssh user@$IP 'bash -s' < src/scripts/resources_info.sh)
+  INFO_NODE=$(ssh user@$IP 'bash -s' < src/scripts/utils/resources_info.sh)
   if [ $FIRST = true ]; then
     FIRST=false
   else
