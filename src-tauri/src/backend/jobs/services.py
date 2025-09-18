a0358b0600643b5e6666082d07e136d972ff7b4d
William Velasco <william.velasco@correounivalle.edu.co>
2025-09-17 23:23:53 -0500

    idea to multiple outputs

--- src-tauri/src/backend/jobs/services.py
+++ src-tauri/src/backend/jobs/services.py
@@ -4,13 +4,26 @@ from fastapi import HTTPException
 from .submit_builder import HTCondorSubmit
 from .schemas import JobData
 
-def create_submit_file_service(job: JobData):
+def create_submit_file_service(job: JobData, output_type: str):
     """
     permits to create the submit file (or classad) to save it in the
-    sideger-jobs directory (shared volume)
+    sideger-jobs directory (shared volume).
+    identifies the job and output type to create his respective classAd.
     """
     fields_filled_submit_file = HTCondorSubmit(**job.model_dump())
     #classad_file = fields_filled_submit_file.to_submit_file()
+
+    # use cluster and process
+    if output_type == "one_directory": # includes outputs with 1...n files (resultados/)
+        # ep 1: queue 01 -> resultados/output_$(process).out
+        # ep 2: queue 50 -> resultados/output_$(process).out
+        pass
+    # use cluster and process
+    elif output_type == "n_directories": # includes outputts with 2...n directories / job files ; job files can use the same name
+        # ep 1: queue 02 -> resultados_$(process)/output.out resumen.txt
+        # ep 2: queue 50 -> resultados_$(process)/output.out resumen.txt
+        pass
+
 
     working_directory = os.path.expanduser("~/sideger-jobs")
     os.makedirs(working_directory, exist_ok=True)
