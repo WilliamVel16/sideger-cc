52f8ab567a5d56a22fe9ad6295883caee2686187
William Velasco <william.velasco@correounivalle.edu.co>
2025-12-02 04:37:26 -0500

    dockerized backend

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,14 +1,19 @@
 use std::process::Command;
+use std::env;
 
 fn main() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
         .setup(|_app| {
-            Command::new("bash")
-                .arg("start.sh")
-                .current_dir("../src-backend")
-                .spawn()
-                .expect("it couldn't start the Backend Python");
+            if env::var("USE_EXTERNAL_BACKEND").unwrap_or("0".into()) != "1" { //USE_EXTERNAL_BACKEND=1 avoid run start.sh (without container)
+                Command::new("bash")
+                    .arg("start.sh")
+                    .current_dir("../src-backend")
+                    .spawn()
+                    .expect("Error starting Python backend");
+            } else {
+                println!("Backend externo detectado. Usando Docker.");
+            }
             Ok(())
         })
         .run(tauri::generate_context!())
